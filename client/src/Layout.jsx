a2dc23141c83fa429522005629829f1ddf19363e
CodeWithAshafak <ashafak04@gmail.com>
2025-02-27 16:28:53 +0530

    added

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import TopMenu from './componets/topmenu'
+// import TopMenu from './componets/topmenu'
+
+
 import {Outlet} from "react-router-dom"
 import Footer from './componets/Footer'
+import TopMenu from './componets/topmenu'
 const Layout = () => {
   return (
     <>
--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8080"
-
+const BASE_URL = "https://doctor-appointment-system-fua6.onrender.com"
+// https://doctor-appointment-system-fua6.onrender.com
 
 
 
