4b5c7d62b6dab84f4bf8dfbaff8ee2af629e6917
CodeWithAshafak <ashafak04@gmail.com>
2025-02-27 16:06:50 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './pages/Home'
-import Searchdoctor from './pages/searchdoctor'
+
+
 import Doctorlogin from './pages/Doctorlogin'
+import Searchdoctor from './pages/Searchdoctor'
 
 
 const App = () => {
@@ -16,7 +18,8 @@ const App = () => {
       <Route path='/' element={<Layout/>}>
       <Route index  element={<Home/>} />
       <Route path='home'  element={<Home/>} />
-      {/* <Route path='searchdoctor'  element={<Searchdoctor/>} /> */}
+      <Route path='searchdoctor'  element={<Searchdoctor/>} />
+
       <Route path='doctorlogin'  element={<Doctorlogin/>} />
      
 
